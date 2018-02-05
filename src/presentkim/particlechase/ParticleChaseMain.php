a630983a3f9eea4e9a2e16232c97a6e4879caec5
PresentKim <qjrmckd@gmail.com>
2018-02-05 14:36:16 +0900

    ParticleChaseMain : Add PoolCommand getter and setter

--- src/presentkim/particlechase/ParticleChaseMain.php
+++ src/presentkim/particlechase/ParticleChaseMain.php
@@ -210,4 +210,18 @@ class ParticleChaseMain extends PluginBase{
         }
         $this->getServer()->getCommandMap()->register(strtolower($this->getName()), $this->command);
     }
+
+    /**
+     * @param string $name = ''
+     *
+     * @return PoolCommand
+     */
+    public function getCommand(string $name = '') : PoolCommand{
+        return $this->command;
+    }
+
+    /** @param PoolCommand $command */
+    public function setCommand(PoolCommand $command) : void{
+        $this->command = $command;
+    }
 }
